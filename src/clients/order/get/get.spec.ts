8c77efb67f139abacf057d15964eb5f105155168
Danielen Cestari Nunes <danielen.cestari@mercadolivre.com>
2024-11-28 15:07:03 -0300

    include integration test

--- src/clients/order/get/get.spec.ts
+++ src/clients/order/get/get.spec.ts
@@ -8,23 +8,22 @@ jest.mock('@utils/restClient');
 describe('Get Order', () => {
 	test('should get Order', async () => {
 		const config = new MercadoPagoConfig({ accessToken: 'access_token' });
-		const order_id = '01JDMS5325ZDWMESRB5G2541BD';
-		
-		// Mock Order Response
-		const MockOrderResponse: OrderResponse = {
+		const orderId = '01JDMS5325ZDWMESRB5G2541BD';
+		const mockOrderResponse: OrderResponse = {
 			api_response: {
 				status: 200,
 				headers: [
 					'Content-Type',['application/json']
 				]
 			},
-			id: order_id,
+			id: orderId,
 			status: '200',
 			total_amount: '200.00',
 		};
-    
-		const spyFetch = jest.spyOn(RestClient, 'fetch').mockResolvedValue(MockOrderResponse);
-		const result = await get({ id: order_id, config });
+		const spyFetch = jest.spyOn(RestClient, 'fetch').mockResolvedValue(mockOrderResponse);
+		
+		const result = await get({ id: orderId, config });
+
 		expect(spyFetch).toHaveBeenCalledWith('/v1/orders/01JDMS5325ZDWMESRB5G2541BD',
 			{
 				method: 'GET',
@@ -34,6 +33,6 @@ describe('Get Order', () => {
 				}
 			}
 		);
-		expect(result).toEqual(MockOrderResponse);
+		expect(result).toEqual(mockOrderResponse);
 	});
 });
