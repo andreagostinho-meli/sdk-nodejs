319afa4ef667a8d49cc71d2c89962f03bc8d8adf
Danielen Cestari Nunes <danielen.cestari@mercadolivre.com>
2024-12-02 19:21:20 -0300

    merge master

--- src/clients/order/get/types.ts
+++ src/clients/order/get/types.ts
@@ -2,7 +2,6 @@ import { MercadoPagoConfig } from '@src/mercadoPagoConfig';
 import type { Options } from '@src/types';
 
 export declare type OrderGetData = {
-export declare type OrderGetData = {
   id: string;
   requestOptions?: Options;
 }
